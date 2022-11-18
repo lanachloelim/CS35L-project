931754f5b5a7066b3d7be2a2268d03c10f15e7eb
Lana Lim <lanachloe@g.ucla.edu>
2022-11-18 13:08:47 -0800

    Updates

---
+++ precache-manifest.9247d799b20a7f2d8b52289d7f73113b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42454dc60e3737b2c5a1",
+    "url": "/lanachloelim/CS35L-project.git/static/css/main.c459a88d.chunk.css"
+  },
+  {
+    "revision": "42454dc60e3737b2c5a1",
+    "url": "/lanachloelim/CS35L-project.git/static/js/main.42454dc6.chunk.js"
+  },
+  {
+    "revision": "ae4700ead3b6e1632dda",
+    "url": "/lanachloelim/CS35L-project.git/static/js/1.ae4700ea.chunk.js"
+  },
+  {
+    "revision": "e4b49debf0b273bc5f79",
+    "url": "/lanachloelim/CS35L-project.git/static/js/2.e4b49deb.chunk.js"
+  },
+  {
+    "revision": "66be52a14ca80797b957",
+    "url": "/lanachloelim/CS35L-project.git/static/js/runtime~main.66be52a1.js"
+  },
+  {
+    "revision": "27c8f09625f06ca74a6251bb7f4c49d8",
+    "url": "/lanachloelim/CS35L-project.git/index.html"
+  }
+];
